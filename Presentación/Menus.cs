d64abe9f0c95f9989f57700a38e26b9daa072b56
JPMC <juanpim344@gmail.com>
2024-08-22 18:40:37 -0500

    Cambio 1 - Me tome una pausa. (22/08/2024)

---
+++ Presentación/Menus.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Logica;
+
+namespace Presentación
+{
+    public class Menus
+    {
+        public void MainMenu() {
+            int Opt;
+            do
+            {
+                Console.SetCursorPosition(10, 5); Console.WriteLine(" --- Menu principal --- ");
+                Console.SetCursorPosition(5, 7); Console.WriteLine(" 1. Ejercicios basicos ");
+                Console.SetCursorPosition(5, 8); Console.WriteLine(" 2. Ejercicios intermedios ");
+                Console.SetCursorPosition(5, 9); Console.WriteLine(" 3. Ejercicios avanzados ");
+                Console.SetCursorPosition(5, 10); Console.WriteLine("0. Salir ");
+                Opt = int.Parse(Console.ReadLine());
+            } while (Opt != 0);
+
+            switch (Opt)
+            {
+                case 1: break;
+                case 2: break;
+                case 3: break;
+                case 4: Opt = 0; break;
+            }
+        }
+
+        public void MenuBasico()
+        {
+            Console.Clear();
+            int Opt = 0;
+            do
+            {
+                Console.SetCursorPosition(10, 5); Console.WriteLine(" --- Ejercicios basicos ---");
+                Console.SetCursorPosition(5, 7); Console.WriteLine("1. Lista ordenada ");
+                Console.SetCursorPosition(5, 8); Console.WriteLine("2. Autentificación de usuario ");
+            } while (Opt != 0);
+        }
+    }
+}
---
+++ Presentación/VisualBasic.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Presentación
+{
+    internal class VisualBasic
+    {
+        public void SortedList()
+        {
+            Console.Clear();
+            List<int> List = new List<int>();
+            char Opt = 'S';
+            int Num;
+            bool Asc = true, Desc = true;
+
+            do {
+                Console.SetCursorPosition(10, 5); Console.WriteLine(" --- Lista Ordenada --- ");
+                Console.SetCursorPosition(5, 7); Console.Write("Ingrese numeros a la lista: ");
+                Num = Console.Read();
+                List.Add(Num);
+                Console.SetCursorPosition(10, 15); Console.WriteLine("¿Desea continuar? (S/N)");
+                Opt = Convert.ToChar(Console.ReadLine());
+                char.ToUpper(Opt);
+            } while ( Opt == 'S' );
+
+            Console.SetCursorPosition(5, 9); Console.WriteLine("Lista de numeros digitada: ");
+
+            foreach (int i in List)
+            {
+                Console.SetCursorPosition(3, 11); Console.Write(i); Console.Write(' ');
+
+                if (List[i] > List[i + 1])
+                {
+                    Asc = false;
+                }
+                if (List[i] < List[i + 1])
+                {
+                    Desc = false;
+                }
+            }
+
+            if (!Asc)
+            {
+                Console.WriteLine("La lista se encuentra ordenada ascendentemente";
+            } else if (Desc) {
+                Console.WriteLine("La lista se encuentra ordenada descendentemente";
+            } else 
+            {
+                Console.WriteLine("La lista no se encuentra ordenada";
+            }
+
+
+        }
+    }
+}
